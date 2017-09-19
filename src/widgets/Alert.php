41155b3a14115d28a4f537e60f25746a4e518585
Vitaliy <theyamshikov@yandex.ru>
2017-09-19 10:51:30 +0600

    add files

---
+++ src/widgets/Alert.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace common\widgets;
+
+use Yii;
+use kartik\widgets\Alert as kartikAlert;
+
+/**
+ * Extends the \yii\bootstrap\Alert widget with additional styling and auto fade out options.
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class Alert extends kartikAlert
+{
+	/**
+	 * @var bool auto fill data
+	 */
+	public $autoFill = true;
+	
+	/**
+	 * Init widget
+	 */
+	public function init()
+	{
+		if(Yii::$app->getResponse()->getStatusCode() != 302) {
+			if($this->autoFill) {
+				$entity = Yii::$app->notify->flash->fetch();
+				if($entity) {
+					$this->type = $entity->type;
+					$this->body = $entity->body;
+				}
+			}
+			if(!empty($this->body)) {
+				parent::init();
+			}
+		}
+	}
+
+	/**
+	 * Runs the widget
+	 */
+	public function run()
+	{
+		if(!empty($this->body)) {
+			parent::run();
+		}
+	}
+	
+	public static function add($body, $type = parent::TYPE_SUCCESS)
+	{
+		Yii::$app->notify->flash->send($body, $type);
+	}
+	
+}
