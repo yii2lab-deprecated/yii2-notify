3a4eef2e134d164d1b4fd40dea32587bb8832750
Vitaliy <theyamshikov@yandex.ru>
2017-11-14 16:11:35 +0600

    rename gate to mock

--- src/domain/repositories/mock/EmailRepository.php
+++ src/domain/repositories/mock/EmailRepository.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace yii2lab\notify\domain\repositories\gate;
+namespace yii2lab\notify\domain\repositories\mock;
 
 use yii2lab\domain\repositories\BaseRepository;
 use yii2lab\notify\domain\entities\MessageEntity;
 
-class SmsRepository extends BaseRepository {
+class EmailRepository extends BaseRepository {
 	
 	public function send(MessageEntity $message) {
-		//prr('--- cron sms ---');
+		//prr('--- cron Email ---');
 		//prr($message);
 	}
 	
--- src/domain/repositories/mock/SmsRepository.php
+++ src/domain/repositories/mock/SmsRepository.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace yii2lab\notify\domain\repositories\gate;
+namespace yii2lab\notify\domain\repositories\mock;
 
 use yii2lab\domain\repositories\BaseRepository;
 use yii2lab\notify\domain\entities\MessageEntity;
 
-class EmailRepository extends BaseRepository {
+class SmsRepository extends BaseRepository {
 	
 	public function send(MessageEntity $message) {
-		//prr('--- cron Email ---');
+		//prr('--- cron sms ---');
 		//prr($message);
 	}
 	
