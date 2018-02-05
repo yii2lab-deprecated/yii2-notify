eb130e9ee12808465c4b96075cec1cc4e2aeaf8c
Vitaliy <theyamshikov@yandex.ru>
2018-02-05 17:39:00 +0600

    fix path

--- src/domain/repositories/session/FlashRepository.php
+++ src/domain/repositories/session/FlashRepository.php
@@ -5,6 +5,7 @@ namespace yii2lab\notify\domain\repositories\session;
 use yii2lab\helpers\ReflectionHelper;
 use yii2lab\domain\repositories\BaseRepository;
 use Yii;
+use yii2lab\helpers\ReflectionHelper;
 use yii2lab\notify\domain\entities\FlashEntity;
 use yii2lab\notify\domain\widgets\Alert;
 
