4aa62fb3b34d155aaf092a111124f24047af4fd1
Vitaliy <theyamshikov@yandex.ru>
2018-08-11 22:19:08 +0600

    upgrade rbac

--- src/admin/Module.php
+++ src/admin/Module.php
@@ -3,12 +3,20 @@
 namespace yii2lab\notify\admin;
 
 use yii\base\Module as YiiModule;
+use yii2lab\notify\domain\enums\NotifyPermissionEnum;
+use yii2lab\helpers\Behavior;
 
 /**
  * dashboard module definition class
  */
 class Module extends YiiModule
 {
-	
-	
+
+    public function behaviors()
+    {
+        return [
+            'access' => Behavior::access(NotifyPermissionEnum::MANAGE),
+        ];
+    }
+
 }
