757bee6f21734ef55cdab890fbc0fb474928bceb
amubarak <amubarak@wooppay.com>
2019-03-14 16:42:38 +0600

    mes

---
+++ src/domain/messages/uz/main.php
@@ -0,0 +1,11 @@
+﻿<?php
+return [
+	'title'=>'Хабарлар',
+	'sms'=>'SMS',
+	'email'=>'Email',
+	'message_success_send' => 'Хабар муваффақиятли жўнатилди',
+	
+	'address' => 'Манзил',
+	'subject' => Мавзу',
+	'content' => 'Хабар',
+];
