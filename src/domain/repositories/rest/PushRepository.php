cf38929544324990de9c0d460d7f6ad2c3c31424
amubarak <amubarak@wooppay.com>
2019-06-13 14:33:44 +0600

    fix error https://youtrack.wooppay.com/issue/PL-597

--- src/domain/repositories/rest/PushRepository.php
+++ src/domain/repositories/rest/PushRepository.php
@@ -3,10 +3,9 @@
 namespace yii2lab\notify\domain\repositories\rest;
 
 use Yii;
-use yii2lab\domain\exceptions\UnprocessableEntityHttpException;
-use yii2lab\domain\helpers\ErrorCollection;
 use yii2lab\notify\domain\entities\PushEntity;
 use yii2lab\notify\domain\interfaces\repositories\PushInterface;
+use yii2lab\rest\domain\entities\ResponseEntity;
 use yii2lab\rest\domain\repositories\base\BaseRestRepository;
 use yii2woop\common\domain\account\v2\entities\FireUserEntity;
 
@@ -17,50 +16,55 @@ use yii2woop\common\domain\account\v2\entities\FireUserEntity;
  *
  * @property \yii2lab\notify\domain\Domain $domain
  */
-class PushRepository extends BaseRestRepository implements PushInterface{
-	
+class PushRepository extends BaseRestRepository implements PushInterface
+{
+
 	/**
 	 * @param $notification
 	 *
 	 * @return bool
 	 */
-	public function sendAll($notification) {
+	public function sendAll($notification)
+	{
 		$pushEntity = $this->prepareSend($notification);
-		
+
 		$usersList = \App::$domain->account->fireUser->all();
 		$targetTokens = [];
-		foreach($usersList as $users) {
-			if(!empty($users->device_token)) {
+		foreach ($usersList as $users) {
+			if (!empty($users->device_token)) {
 				$targetTokens[] = $users->device_token;
 			}
 		}
 		$fireBase = new FireBaseSend();
 		return $fireBase->sendMultiple($targetTokens, $pushEntity->toArray());
 	}
-	
+
 	/**
 	 * @param $notification
 	 *
+	 * @return bool
 	 * @var FireUserEntity $fireUser
 	 *
-	 * @return bool
 	 */
-	public function send($id, $notification) {
+	public function send($id, $notification)
+	{
 		$pushEntity = $this->prepareSend($notification);
 		$fireUser = \App::$domain->account->fireUser->oneById($id);
 		$fireBase = new FireBaseSend();
 		return $fireBase->send('dYxtWvqaR1A:APA91bEIhkotwlCtW0ld-7A22U3bMM5kMI4MPd8lahQkmo95hpO_zukxDrKY9fwcYutud5DufyeE-g-9L9BoWycXJC1CravdFNR-wTUnYjvTyAtuJBKOmL4vgd2LD1FIGUtbuCwiokaR'
 			, $pushEntity->toArray());
 	}
-	
-	private function prepareSend($notification){
+
+	private function prepareSend($notification)
+	{
 		$pushEntity = $this->forgeEntity($notification);
 		$pushEntity->validate();
 		return $pushEntity;
 	}
-	
+
 	// todo:not work. function makes curl request to gcm servers
-	private function sendPushNotification($fields) {
+	private function sendPushNotification($fields)
+	{
 		$api_key = Yii::$app->params['FIRE_BASE_PROJECT_NAME'];
 		// Set POST variables
 		$url = 'https://fcm.googleapis.com/fcm/send';
@@ -72,10 +76,21 @@ class PushRepository extends BaseRestRepository implements PushInterface{
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_SSL_VERIFYPEER => false,
 		]);
-		if($result->hasErrors()){
-			prr($result,1,1);
+		if ($result->hasErrors()) {
+			prr($result, 1, 1);
 		}
-		
+
 		return true;
+	}
+
+	public function forgeEntity($data, $class = PushEntity::class)
+	{
+		if ($data instanceof ResponseEntity) {
+			$data = $data->data;
+		} elseif (empty($data)) {
+			$data = [];
+			return $this->domain->factory->entity->create($class, $data);
+		}
+		return parent::forgeEntity($data, $class);
 	}
 }
